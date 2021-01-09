e9cf3d46c2609e2dc6fdb145efc5fd51566b6f44
Djaizansi <youcef.jallali@gmail.com>
2021-01-09 23:47:29 +0100

    [DONE] Complétion profil après inscription + système accès role

--- app/src/Controller/Profile/CompleteRegisterController.php
+++ app/src/Controller/Profile/CompleteRegisterController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller\Profile;
 
+use App\Entity\Adherent;
+use App\Form\AdherentType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CompleteRegisterController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/profil/complete-inscription", name="register")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('profile/index.html.twig');
+        $user = $this->getUser();
+        if($this->isGranted('ROLE_ADH')){
+            $adherent = new Adherent();
+            $form = $this->createForm(AdherentType::class, $adherent);
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()){
+                $adherent = $form->getData();
+                $adherent->setUserAccount($this->getUser());
+
+                $user->setRoles(['ROLE_ADH_CONFIRME']);
+                $this->em->persist($adherent);
+                $this->em->flush();
+
+                return $this->redirectToRoute('default_index');
+            }
+        }else{
+            echo "wowowowo";
+        }
+        return $this->render('profile/index.html.twig',[
+            'form' => $form->createView()
+        ]);
     }
 }
