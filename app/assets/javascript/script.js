567695538f31341c2134463448deac4bee5c51b8
Djaizansi <youcef.jallali@gmail.com>
2021-02-07 03:51:01 +0100

    [DONE] Modification prénom AJAX + IziToast with JsonReponse

--- app/assets/javascript/script.js
+++ app/assets/javascript/script.js
@@ -14,6 +14,74 @@ window.setTimeout(function () {
 
 $(window).scroll(function(e) {
     const distanceScrolled = $(this).scrollTop();
-    console.log(distanceScrolled);
     $('.banner-photo div:first-child').css('-webkit-filter', 'blur('+distanceScrolled/40+'px)');
+});
+
+const addInput = function(){
+    $(this).css({
+        'background': 'rgba(238,238,238,0.7)',
+        'border-radius': '5px',
+        'padding': '3px'
+    });
+}
+
+$('.banner-photo div:last-child p').click(addInput);
+function changePrestation(id) {
+    var choix = $('#categorieFirst' + id).val();
+    $.ajax({
+        url: '/getPrestation',
+        type: 'GET',
+        data: 'choix=' + choix,
+        success: function(data) {
+            if (data) {
+                var tab = $.parseJSON(data);
+                var html;
+                html = '<option default selected disabled>Prestation (' + tab.length + ')</option>' + '<option disabled>──────────</option>';
+                for (var i = 0; i < tab.length; i++) {
+                    html = html + '<option value=' + tab[i][0] + '>' + tab[i][1] + '</option>';
+
+                }
+                $('#prestationFirst' + id).html(html);
+            }
+        }
+    });
+}
+
+const getIziToast = function(title,message,icon,color){
+    iziToast.show({
+        icon: `${icon}`,
+        color: `${color}`,
+        title: `${title}`,
+        message: `${message}`,
+    });
+}
+
+$(document).click(function(event) {
+    const target = $('.banner-photo div:last-child p');
+    if(target.attr('style') !== undefined){
+        //Closest permet de récuperer le premier ancestre de l'element passer
+        // return proche de l'element courant ou l'élement courant.
+        if(!$(event.target).closest(target).length){
+            target.removeAttr('style');
+            const newName = target.text();
+            $.ajax({
+                url: '/edit-nom',
+                type: 'POST',
+                data: {name: newName},
+                success: function(data) {
+                    if(data['title'] === 'OK'){
+                        getIziToast('OK','Modification réussie !','fas fa-check','green');
+                    }else if(data['title'] === 'Warning'){
+                        getIziToast('Attention','Aucune modification établie car le prénom est le même !','fas fa-exclamation-triangle','orange');
+                    }else if(data['title'] === 'Error'){
+                        getIziToast('Erreur','Le nom ne peut pas être vide','fas fa-ban','red');
+                        target.text(data['data']);
+                    }
+                },
+                error: function(data){
+                    getIziToast('Attention','Aucune modification établie car le prénom est le même !','fas fa-ban','red');
+                }
+            });
+        }
+    }
 });
--- app/src/Controller/Profile/ProfileController.php
+++ app/src/Controller/Profile/ProfileController.php
@@ -2,12 +2,32 @@
 
 namespace App\Controller\Profile;
 
+use App\Entity\Adherent;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em){
+        $this->em = $em;
+    }
+
+    protected function getArrayMessage($title, $data=null){
+        $array = [
+            'title' => $title,
+        ];
+        if(!is_null($data)){
+            $array['data'] = $data;
+        }
+        return $array;
+    }
+
     /**
      * @Route("/mon-compte", name="account")
      */
@@ -16,5 +36,27 @@ class ProfileController extends AbstractController {
         $user = $this->getUser();
         $hasRole = $this->isGranted('ROLE_ADH');
         return $this->render('profile/account.html.twig');
+    }
+
+    /**
+     * @Route("/edit-nom", name="edit_name")
+     */
+    public function editName(Request $request): Response
+    {
+        if($request->isXmlHttpRequest()){
+            $name = $request->request->get('name');
+            $currentUser = $this->getUser();
+            $adherent = $this->em->getRepository(Adherent::class)->findById($currentUser->getAdherent())[0];
+            if($adherent->getFirstname() === $name){
+                return new JsonResponse($this->getArrayMessage('Warning'));
+            }
+            if(empty($name)){
+                return new JsonResponse($this->getArrayMessage('Error',$adherent->getFirstname()));
+            }
+            $adherent->setFirstname($name);
+            $this->em->flush();
+            return new JsonResponse($this->getArrayMessage('OK'));
+        }
+        return new JsonResponse('Une erreur est survenue',500);
     }
 }
