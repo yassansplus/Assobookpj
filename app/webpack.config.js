7f0e74fc9f2690c0da107faadd7fd3f823e193e3
Marwane BOUABDELLAH <56023597+BOUABDELLAHM@users.noreply.github.com>
2021-02-08 15:10:10 +0100

    Rajout de la template "Who Are We" (#20)
    * Ajout de la route pour la template WAW
    
    * [FIX] Route WAW
    
    * Ajout de l'entrypoint wawFront
    
    * Ajout du fichier SCSS pour la template WAW
    
    * [DONE] Template WAW
    
    * Ajout d'une nouvelle couleur (low-red)
    
    * [RESOLVE] Applications des correctifs CSS
    
    * [RESOLVE] Applications des correctifs HTML/CSS

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -25,6 +25,7 @@ Encore
     .addStyleEntry('global','./assets/styles/global.scss')
     .addStyleEntry('formAuth', './assets/styles/formAuth.scss')
     .addStyleEntry('index', './assets/styles/index.scss')
+    .addStyleEntry('wawFront','./assets/styles/wawFront.scss')
     .addStyleEntry('banner', './assets/styles/component/banner.scss')
     .addStyleEntry('sidebar', './assets/styles/component/sidebar.scss')
 
