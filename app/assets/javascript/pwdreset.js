4f6c0def9c2c0324f2b17b0442f99c618a5a1e6d
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2021-02-09 23:40:25 +0100

    Ajout du systeme de match Par tag et de follow entre les User et les association, un utilisateur est desormais capable de suivre une association

--- app/assets/javascript/pwdreset.js
+++ app/assets/javascript/pwdreset.js
@@ -4,12 +4,17 @@ window.iziToast = require('izitoast');
 const resetpwd = $('.upd-pwd');
 const cancelpwd = $('.btn-secondary');
 
-const updatePwd = function(){
+const updatePwd = function () {
     $(this).addClass('d-none');
-    $('.sidebar div:last-child').removeClass('d-none');
+    $('.formPassword').removeClass('d-none');
+}
+const cancelForm = function () {
+    $('form[name=update_pwd]')[0].reset();
+    $('.sidebar .formPwd').addClass('d-none')
+    resetpwd.removeClass('d-none');
 }
 
-const getIziToast = function(title,message,icon,color){
+const getIziToast = function (title, message, icon, color) {
     iziToast.show({
         icon: `${icon}`,
         color: `${color}`,
@@ -18,25 +23,20 @@ const getIziToast = function(title,message,icon,color){
     });
 }
 
-const cancelForm = function(){
-    $('form[name=update_pwd]')[0].reset();
-    $('.sidebar div:last-child').addClass('d-none');
-    resetpwd.removeClass('d-none');
-}
 
-$('form[name=update_pwd]').submit(function(event){
+$('form[name=update_pwd]').submit(function (event) {
     // cancels the form submission
     event.preventDefault();
     submitForm();
 });
 
-const condition = function(data,compare,title,message,icon,color){
-    if(data === compare){
-        return getIziToast(title,message,icon,color);
+const condition = function (data, compare, title, message, icon, color) {
+    if (data === compare) {
+        return getIziToast(title, message, icon, color);
     }
 }
 
-const submitForm = function(){
+const submitForm = function () {
     const numberOfInput = $('form[name=update_pwd] input[type=password]').length;
     const oldpwd = $('#update_pwd_old_password');
     const newpwd = $('#update_pwd_new_password_first');
@@ -47,20 +47,20 @@ const submitForm = function(){
         type: "POST",
         url: "/reset-password",
         data: {
-            input:numberOfInput,
-            oldpwd:oldpwd.val(),
-            newpwd:newpwd.val(),
-            confirmpwd:confirmpwd.val(),
-            required:numberOfRequired
+            input: numberOfInput,
+            oldpwd: oldpwd.val(),
+            newpwd: newpwd.val(),
+            confirmpwd: confirmpwd.val(),
+            required: numberOfRequired
         },
-        success : function(data){
-            condition(data,'OK','Super','Mot de passe modifié !','fas fa-check','green');
-            condition(data,'Pwd','Erreur','Mot de passe incorrect !','fas fa-ban','red');
-            condition(data,'Input','Erreur','Mot de passe modifié !','fas fa-ban','red');
-            condition(data,'Equals','Erreur','Les mots de passe ne correspondent pas !','fas fa-ban','red');
-            condition(data,'Size','Attention','Votre mot de passe doit faire entre 8 et 16 caractères','fas fa-exclamation-triangle','orange');
-            condition(data,'Error','Erreur','Une erreur est survenue','fas fa-ban','red');
-            if(data === 'OK'){
+        success: function (data) {
+            condition(data, 'OK', 'Super', 'Mot de passe modifié !', 'fas fa-check', 'green');
+            condition(data, 'Pwd', 'Erreur', 'Mot de passe incorrect !', 'fas fa-ban', 'red');
+            condition(data, 'Input', 'Erreur', 'Mot de passe modifié !', 'fas fa-ban', 'red');
+            condition(data, 'Equals', 'Erreur', 'Les mots de passe ne correspondent pas !', 'fas fa-ban', 'red');
+            condition(data, 'Size', 'Attention', 'Votre mot de passe doit faire entre 8 et 16 caractères', 'fas fa-exclamation-triangle', 'orange');
+            condition(data, 'Error', 'Erreur', 'Une erreur est survenue', 'fas fa-ban', 'red');
+            if (data === 'OK') {
                 cancelForm();
             }
         }
