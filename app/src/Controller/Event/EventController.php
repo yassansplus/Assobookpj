0870064d74db12b8ebb5462e8981847b8f32534e
Wassim <wassimdah@gmail.com>
2021-02-10 04:37:22 +0100

    [BACK] Commit before rebase - Adding publication properties in event

--- app/src/Controller/Event/EventController.php
+++ app/src/Controller/Event/EventController.php
@@ -3,12 +3,17 @@
 namespace App\Controller\Event;
 
 use App\Entity\Event;
+use App\Entity\Publication;
 use App\Form\EventType;
+use App\Form\UpdatePwdType;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 /**
  * @Route("/event")
@@ -20,22 +25,29 @@ class EventController extends AbstractController
      */
     public function index(EventRepository $eventRepository): Response
     {
+        $form = $this->createForm(UpdatePwdType::class);
+
         return $this->render('event/index.html.twig', [
             'events' => $eventRepository->findAll(),
+            'form' => $form->createView()
         ]);
     }
 
     /**
      * @Route("/new", name="event_new", methods={"GET","POST"})
+     * @IsGranted("ROLE_ASSOC_CONFIRME")
+     * @param Request $request
+     * @return Response
      */
     public function new(Request $request): Response
     {
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
+            $event->getPublication()->setAssociation($this->getUser()->getAssociation());
+            $event->getPublication()->setDatePublication();
             $entityManager->persist($event);
             $entityManager->flush();
 
@@ -50,8 +62,11 @@ class EventController extends AbstractController
 
     /**
      * @Route("/{id}", name="event_show", methods={"GET"})
+     * @param Event $event
+     * @param EntityManagerInterface $em
+     * @return Response
      */
-    public function show(Event $event): Response
+    public function show(Event $event, EntityManagerInterface $em): Response
     {
         return $this->render('event/show.html.twig', [
             'event' => $event,
