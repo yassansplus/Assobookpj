2e330e5d8a696e804f341bbc2691bed22a35294d
Marwane <manbou92@hotmail.fr>
2021-07-23 15:01:53 +0200

    Add class input

--- app/src/Form/PublicationType.php
+++ app/src/Form/PublicationType.php
@@ -6,6 +6,7 @@ use App\Entity\Association;
 use App\Entity\Publication;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,8 +18,12 @@ class PublicationType extends AbstractType
     {
         $builder
             ->add('description', TextareaType::class, [
-                'label' => 'Description',
-                'required' => true
+                'label' => 'Ajouter une publication',
+                'required' => false,
+                'attr' => ['class' => 'form-control p-2', 'rows' => '3', 'placeholder' => 'Quoi de neuf ?']
+            ])
+            ->add('publier', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-success publier'],
             ])
         ;
     }
